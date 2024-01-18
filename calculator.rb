75313d02f8f8e828c0c81daacff46704e4436d43
odmrs <omarcosviniciusdev@gmail.com>
2024-01-18 16:42:53 -0300

    add: treat the options, and show the result between the operations

--- calculator.rb
+++ calculator.rb
@@ -12,18 +12,69 @@ puts "0 - Exit"
 print "\nEnter an option above: "
 option = gets.chomp.to_i
 
+system 'clear'
+
+
+system 'clear'
+
 # Treat this option
 
+if option == 1
+  puts "=" * 5 + " Addtion " + "=" * 5
+
+  print "\nEnter the first number: "
+  firstNumber = gets.chomp.to_i 
+
+  print "Enter the second number: "
+  secondNumber = gets.chomp.to_i 
+
+  result = firstNumber + secondNumber
+  puts "\nThe sum between -> #{firstNumber} + #{secondNumber} = #{result}"
+  gets
+elsif option == 2
+  puts "=" * 5 + " Subtraction " + "=" * 5
+
+  print "\nEnter the first number: "
+  firstNumber = gets.chomp.to_i 
+
+  print "Enter the second number: "
+  secondNumber = gets.chomp.to_i 
+
+  result = firstNumber - secondNumber
+  puts "\nThe subtraction between -> #{firstNumber} - #{secondNumber} = #{result}"
+  gets
+elsif option == 3
+  puts "=" * 5 + " Multiplication " + "=" * 5
+
+  print "\nEnter the first number: "
+  firstNumber = gets.chomp.to_i 
+
+  print "Enter the second number: "
+  secondNumber = gets.chomp.to_i 
+
+  result = firstNumber * secondNumber
+
+  puts "\nThe multiplication between -> #{firstNumber} x #{secondNumber} = #{result}"
+  gets 
+elsif option == 4
+  puts "=" * 5 + " Division " + "=" * 5
+
+  print "\nEnter the first number: "
+  firstNumber = gets.chomp.to_f 
+
+  print "Enter the second number: "
+  secondNumber = gets.chomp.to_f 
+
+  result = firstNumber / secondNumber
+
+  puts "\nThe division between -> #{firstNumber} / #{secondNumber} = #{result}"
+  gets 
+elsif option == 0
+  puts "=" * 5 + " Thanks for use my Calculator " + "=" * 5
+  gets
+else
+  puts "The options is not found"
+  gets 
+end
+
 system "clear"
-
-# Operations
-
-print "Enter the first number: "
-firstNumber = gets.chomp.to_i 
-
-print "Enter the second number: "
-secondNumber = gets.chomp.to_i 
-
-result = firstNumber + secondNumber
-
-puts "The sum between -> #{firstNumber} + #{secondNumber} = #{result}"
