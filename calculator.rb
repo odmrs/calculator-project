525ad3dff6428c05250ac51495860ad5a060bd1f
odmrs <omarcosviniciusdev@gmail.com>
2024-01-18 10:42:41 -0300

    add: Draw menu, and get the option of this menu

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,22 @@
-system('clear')
+system "clear"
+
+# Menu
+
+puts "=" * 5 + " Calculator " + "=" * 5
+puts "\n1 - Addition"
+puts "2 - Subtraction"
+puts "3 - Multiplication"
+puts "4 - Division"
+puts "0 - Exit"
+
+print "\nEnter an option above: "
+option = gets.chomp.to_i
+
+# Treat this option
+
+system "clear"
+
+# Operations
 
 print "Enter the first number: "
 firstNumber = gets.chomp.to_i 
