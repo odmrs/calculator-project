1b1de72f3fbe4b59926bb79c5fe03462e6b9f2f4
odmrs <omarcosviniciusdev@gmail.com>
2024-01-18 18:16:35 -0300

    feat: replacing the numbers in case to ENUMS

--- calculator.rb
+++ calculator.rb
@@ -9,14 +9,24 @@ puts "3 - Multiplication"
 puts "4 - Division"
 puts "0 - Exit"
 
+options = {
+  add: 1,
+  sub: 2,
+  mult: 3,
+  div: 4,
+  exit: 0
+}
+
 print "\nEnter an option above: "
 option = gets.chomp.to_i
+
+
 
 system 'clear'
 
 # Treat this option
 case option
-when 1
+when options[:add]
   puts "=" * 5 + " Addtion " + "=" * 5
 
   print "\nEnter the first number: "
@@ -28,7 +38,7 @@ when 1
   result = firstNumber + secondNumber
   puts "\nThe sum between -> #{firstNumber} + #{secondNumber} = #{result}"
   gets
-when 2
+when options[:sub]
   puts "=" * 5 + " Subtraction " + "=" * 5
 
   print "\nEnter the first number: "
@@ -40,7 +50,7 @@ when 2
   result = firstNumber - secondNumber
   puts "\nThe subtraction between -> #{firstNumber} - #{secondNumber} = #{result}"
   gets
-when 3
+when options[:mult]
   puts "=" * 5 + " Multiplication " + "=" * 5
 
   print "\nEnter the first number: "
@@ -53,7 +63,7 @@ when 3
 
   puts "\nThe multiplication between -> #{firstNumber} x #{secondNumber} = #{result}"
   gets 
-when 4
+when options[:div]
   puts "=" * 5 + " Division " + "=" * 5
 
   print "\nEnter the first number: "
@@ -66,7 +76,7 @@ when 4
 
   puts "\nThe division between -> #{firstNumber} / #{secondNumber} = #{result}"
   gets 
-when 0
+when options[:exit]
   puts "=" * 5 + " Thanks for use my Calculator " + "=" * 5
   gets
 else
