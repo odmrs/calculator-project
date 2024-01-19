00c8218baf019cf850b350b9756641bf95ea51f3
odmrs <omarcosviniciusdev@gmail.com>
2024-01-19 15:38:01 -0300

    refactor: change uperCase by snake_case

--- calculator.rb
+++ calculator.rb
@@ -1,92 +1,89 @@
 loop do
 
+  system "clear"
 
-system "clear"
+  # Menu
 
-# Menu
+  puts "=" * 5 + " Calculator " + "=" * 5
+  puts "\n1 - Addition"
+  puts "2 - Subtraction"
+  puts "3 - Multiplication"
+  puts "4 - Division"
+  puts "0 - Exit"
 
-puts "=" * 5 + " Calculator " + "=" * 5
-puts "\n1 - Addition"
-puts "2 - Subtraction"
-puts "3 - Multiplication"
-puts "4 - Division"
-puts "0 - Exit"
+  menu_options = {
+    add: 1,
+    sub: 2,
+    mult: 3,
+    div: 4,
+    exit: 0
+  }
 
-options = {
-  add: 1,
-  sub: 2,
-  mult: 3,
-  div: 4,
-  exit: 0
-}
+  print "\nEnter an option above: "
+  user_option = gets.chomp.to_i
 
-print "\nEnter an option above: "
-option = gets.chomp.to_i
+  system 'clear'
 
+  # Treat this option
+  case user_option
+  when menu_options[:add]
+    puts "=" * 5 + " Addtion " + "=" * 5
 
+    print "\nEnter the first number: "
+    first_number = gets.chomp.to_i 
 
-system 'clear'
+    print "Enter the second number: "
+    second_number = gets.chomp.to_i 
 
-# Treat this option
-case option
-when options[:add]
-  puts "=" * 5 + " Addtion " + "=" * 5
+    result = first_number + second_number
+    puts "\nThe sum between -> #{first_number} + #{second_number} = #{result}"
+    gets
+  when menu_options[:sub]
+    puts "=" * 5 + " Subtraction " + "=" * 5
 
-  print "\nEnter the first number: "
-  firstNumber = gets.chomp.to_i 
+    print "\nEnter the first number: "
+    first_number = gets.chomp.to_i 
 
-  print "Enter the second number: "
-  secondNumber = gets.chomp.to_i 
+    print "Enter the second number: "
+    second_number = gets.chomp.to_i 
 
-  result = firstNumber + secondNumber
-  puts "\nThe sum between -> #{firstNumber} + #{secondNumber} = #{result}"
-  gets
-when options[:sub]
-  puts "=" * 5 + " Subtraction " + "=" * 5
+    result = first_number - second_number
+    puts "\nThe subtraction between -> #{first_number} - #{second_number} = #{result}"
+    gets
+  when menu_options[:mult]
+    puts "=" * 5 + " Multiplication " + "=" * 5
 
-  print "\nEnter the first number: "
-  firstNumber = gets.chomp.to_i 
+    print "\nEnter the first number: "
+    first_number = gets.chomp.to_i 
 
-  print "Enter the second number: "
-  secondNumber = gets.chomp.to_i 
+    print "Enter the second number: "
+    second_number = gets.chomp.to_i 
 
-  result = firstNumber - secondNumber
-  puts "\nThe subtraction between -> #{firstNumber} - #{secondNumber} = #{result}"
-  gets
-when options[:mult]
-  puts "=" * 5 + " Multiplication " + "=" * 5
+    result = first_number * second_number
 
-  print "\nEnter the first number: "
-  firstNumber = gets.chomp.to_i 
+    puts "\nThe multiplication between -> #{first_number} x #{second_number} = #{result}"
+    gets 
+  when menu_options[:div]
+    puts "=" * 5 + " Division " + "=" * 5
 
-  print "Enter the second number: "
-  secondNumber = gets.chomp.to_i 
+    print "\nEnter the first number: "
+    first_number = gets.chomp.to_f 
 
-  result = firstNumber * secondNumber
+    print "Enter the second number: "
+    second_number = gets.chomp.to_f 
 
-  puts "\nThe multiplication between -> #{firstNumber} x #{secondNumber} = #{result}"
-  gets 
-when options[:div]
-  puts "=" * 5 + " Division " + "=" * 5
+    result = first_number / second_number
 
-  print "\nEnter the first number: "
-  firstNumber = gets.chomp.to_f 
-
-  print "Enter the second number: "
-  secondNumber = gets.chomp.to_f 
-
-  result = firstNumber / secondNumber
-
-  puts "\nThe division between -> #{firstNumber} / #{secondNumber} = #{result}"
-  gets 
-when options[:exit]
-  puts "=" * 5 + " Thanks for use my Calculator " + "=" * 5
-  gets
-  break
-else
-  puts "The options is not found"
-  gets 
+    puts "\nThe division between -> #{first_number} / #{second_number} = #{result}"
+    gets 
+  when menu_options[:exit]
+    puts "=" * 5 + " Thanks for use my Calculator " + "=" * 5
+    gets
+    system "clear"
+    break
+  else
+    puts "The options is not found"
+    gets 
+  end
 end
-
 system "clear"
-end
